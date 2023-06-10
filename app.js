0dd90dce8af46d372326fe7fd78e1055077c6de2
UmangThakur15 <umangthakuremail@gmail.com>
2023-06-10 17:34:03 -0400

    Assign 5 2.8

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require('express')
+import express from 'express';
 const app = express()
-app.get('/hello', (req, res) => {res.send('Hello World!')})
-
+app.get('/hello', (req, res) => {res.send('Life is good!')})
+app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
 app.listen(4000)
