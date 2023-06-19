d5d4b8aaba2f63ab81e33e9e69862ce6b93e04cb
UmangThakur15 <umangthakuremail@gmail.com>
2023-06-19 19:55:21 -0400

    Assignment 6 commit 17

--- app.js
+++ app.js
@@ -2,35 +2,42 @@ import express from 'express';
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
-import cors from "cors";
 import session from "express-session";
-import mongoose from "mongoose";
-
-mongoose.connect(
-  "mongodb+srv://umangthakuremail:Mongo1511@cluster0.ghe9flu.mongodb.net/tuiter?retryWrites=true&w=majority"
-);
-// mongoose.connect("mongodb://127.0.0.1:27017/tuiter-su1-23");
+import AuthController from "./users/auth-controller.js";
+import cors from 'cors';
 
 const app = express();
+
+import mongoose from "mongoose";
+
+//const CONNECTION_STRING = "mongodb+srv://dhyeysavaliya2000:dhyey123@cluster0.exksxhm.mongodb.net/users?retryWrites=true&w=majority" || 'mongodb://127.0.0.1:27017/tuiter'; 
+const CONNECTION_STRING = "mongodb+srv://umangthakuremail:Mongo1511@cluster0.ghe9flu.mongodb.net/tuiter?retryWrites=true&w=majority" || 'mongodb://127.0.0.1:27017/tuiter'; 
+// const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter'; 
+
+console.log(process.env.DB_CONNECTION_STRING);
+console.log(CONNECTION_STRING);
+// const CONNECTION_STRING = "mongodb+srv://dhyeysavaliya2000:<password>@cluster0.exksxhm.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(CONNECTION_STRING);
+
 
 app.use(
     session({
       secret: "any string",
       resave: false,
-      proxy: true,
-      saveUninitialized: false,
-      cookie:{
-        sameSite: "none",
-        secure:true,
-      },
+      saveUninitialized: true,
+      cookie: {
+        sameSite : "none",
+        secure : true
+      }
     })
    );
    
 app.use(
     cors({
       credentials: true,
-      origin: "https://fastidious-dragon-93ae89.netlify.app",
+      // origin: "https://luxury-mochi-12313b.netlify.app",
     // origin : "http://localhost:3000",
+    origin: "https://a6--fanciful-tapioca-ea98bc.netlify.app"
     })
    );
    
