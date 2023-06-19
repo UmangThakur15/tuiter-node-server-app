b4d548742048f1bf159daaf4502768a61c8476f5
UmangThakur15 <umangthakuremail@gmail.com>
2023-06-19 19:46:30 -0400

    Assignment 6 commit 15

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ import UserController from "./users/users-controller.js";
 import UsersController from "./users/users-controller.js";
 import AuthenticationController from "./users/auth-controller.js";
 import TuitsController from "./tuits/tuits-controller.js";
-import express from "express";
 import cors from "cors";
 import session from "express-session";
 import mongoose from "mongoose";
