67851da1e8fbe479e71502697b826ee03555563a
UmangThakur15 <umangthakuremail@gmail.com>
2023-06-19 18:49:21 -0400

    Assignment 6 commit 4

--- app.js
+++ app.js
@@ -4,37 +4,24 @@ import UserController from "./users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import session from "express-session";
 import AuthController from "./users/auth-controller.js";
-import cors from 'cors';
-
+import cors from 'cors'
 const app = express();
 
-import mongoose from "mongoose";
-
-
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter'
-
-console.log(process.env.DB_CONNECTION_STRING);
-console.log(CONNECTION_STRING);
-mongoose.connect(CONNECTION_STRING);
-
+ 
 
 app.use(
     session({
       secret: "any string",
       resave: false,
       saveUninitialized: true,
-      cookie: {
-        sameSite : "none",
-        secure : true
-      }
     })
    );
    
 app.use(
     cors({
       credentials: true,
+      origin: "https://fastidious-dragon-93ae89.netlify.app",
     // origin : "http://localhost:3000",
-    origin: "https://fastidious-dragon-93ae89.netlify.app"
     })
    );
    
