3cdaa6a4c67441d55f7994e0b8fbb3094bbe1ddb
UmangThakur15 <umangthakuremail@gmail.com>
2023-06-19 19:42:15 -0400

    Assignment 6 commit 13

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 import express from 'express';
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./users/users-controller.js";
-import TuitsController from "./controllers/tuits/tuits-controller.js";
+import UsersController from "./users/users-controller.js";
+import AuthenticationController from "./users/auth-controller.js";
+import TuitsController from "./tuits/tuits-controller.js";
+import express from "express";
+import cors from "cors";
 import session from "express-session";
-import AuthController from "./users/auth-controller.js";
-import cors from 'cors'
-const app = express();
+import mongoose from "mongoose";
 
- 
+mongoose.connect(
+  "mongodb+srv://umangthakuremail:Mongo1511@cluster0.ghe9flu.mongodb.net/tuiter?retryWrites=true&w=majority"
+);
+// mongoose.connect("mongodb://127.0.0.1:27017/tuiter-su1-23");
 
 app.use(
     session({
