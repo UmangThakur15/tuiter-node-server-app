48153265c85209185a903cee8608c84d2affa424
UmangThakur15 <umangthakuremail@gmail.com>
2023-06-20 14:43:36 -0400

    check 27

--- users/users-schema.js
+++ users/users-schema.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const usersSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  firstName: { type: String },
-  lastName: { type: String },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
 }, { collection: "users" });
 export default usersSchema;
