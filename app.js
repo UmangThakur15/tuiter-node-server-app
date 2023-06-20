e056f6924552e87e7e9daa1027440f411ae8b6b2
UmangThakur15 <umangthakuremail@gmail.com>
2023-06-19 23:44:05 -0400

    check 25

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 import mongoose from "mongoose";
 
 //const CONNECTION_STRING = "mongodb+srv://dhyeysavaliya2000:dhyey123@cluster0.exksxhm.mongodb.net/users?retryWrites=true&w=majority" || 'mongodb://127.0.0.1:27017/tuiter'; 
-const CONNECTION_STRING = "mongodb+srv://umangthakuremail:Mongo1511@cluster0.ghe9flu.mongodb.net/users?retryWrites=true&w=majority" || 'mongodb://127.0.0.1:27017/tuiter'; 
+const CONNECTION_STRING = "mongodb+srv://umangthakuremail:Mongo1511@cluster0.ghe9flu.mongodb.net/tuits?retryWrites=true&w=majority" || 'mongodb://127.0.0.1:27017/tuiter'; 
 // const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter'; 
 
 console.log(process.env.DB_CONNECTION_STRING);
